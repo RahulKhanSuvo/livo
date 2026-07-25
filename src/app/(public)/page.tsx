819205b20e55dedc136090b1646424a0c7fd2edd
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2026-07-26 00:58:02 +0600

    feat: add EditorialGrid component to home page

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from '@/components/home/Hero';
 import DepartmentGrid from '@/components/home/DepartmentGrid';
+import EditorialGrid from '@/components/home/EditorialGrid';
 
 export default function Home() {
   return (
     <section>
       <HeroSection />
       <DepartmentGrid />
+      <EditorialGrid />
     </section>
   );
 }
