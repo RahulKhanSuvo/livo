69ea8bc2fef2fd1011811649ad985bbe51279a93
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2026-07-25 23:13:59 +0600

    feat: add hero section data constants

---
+++ src/components/home/data.ts
@@ -0,0 +1,36 @@
+export const heroData = [
+  {
+    title: 'Art Of Style',
+    subtitle: 'Design for modern living',
+    cts: [
+      {
+        label: 'Explore Collections',
+        href: '/collections',
+      },
+      {
+        label: 'Shop',
+        href: '/shop',
+      },
+    ],
+  },
+  {
+    title: 'Urban Sophistication',
+    subtitle: '',
+    cts: [
+      {
+        label: 'Explore Collections',
+        href: '/collections',
+      },
+    ],
+  },
+  {
+    title: 'Quite luxurious',
+    subtitle: 'shape your atmosphere',
+    cts: [
+      {
+        label: 'Shop Lighting',
+        href: '/lanp',
+      },
+    ],
+  },
+];
