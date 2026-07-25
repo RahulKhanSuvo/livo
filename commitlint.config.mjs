60a0b8171609f6c12fb35b83749a483ea2239fc3
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2026-07-25 20:21:55 +0600

    chore: add commitlint configuration

---
+++ commitlint.config.mjs
@@ -0,0 +1,31 @@
+const config = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'build',
+        'ci',
+        'chore',
+        'revert',
+        'wip',
+        'release',
+        'ui',
+      ],
+    ],
+    'header-max-length': [2, 'always', 200],
+    'header-case': [0],
+    'subject-case': [0],
+    'type-case': [0],
+  },
+};
+
+export default config;
