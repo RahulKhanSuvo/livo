719fe93a32a348ab3cc8eb3d4b5c082c78fe4f6f
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2026-07-26 01:04:44 +0600

    ui: adjust EditorialGrid layout columns
    Update the featured item column span to 8 and remove the fixed max-width
    from the container to allow for better responsive scaling.

--- src/components/home/EditorialGrid.tsx
+++ src/components/home/EditorialGrid.tsx
@@ -9,11 +9,11 @@ export const EditorialGrid = () => {
   const secondaryItems = editorialBanners.filter((item) => !item.isFeatured);
 
   return (
-    <Container className="w-full max-w-[1900px]">
+    <Container>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-12 gap-2 md:gap-2 h-auto lg:h-155">
         {/* Large Featured Card (Left Side / Top on Mobile) */}
         {featuredItem && (
-          <div className="md:col-span-2 lg:col-span-7 h-95 sm:h-112.5 md:h-125 lg:h-full">
+          <div className="md:col-span-2 lg:col-span-8 h-95 sm:h-112.5 md:h-125 lg:h-full">
             <BannerCard item={featuredItem} isFeatured />
           </div>
         )}
