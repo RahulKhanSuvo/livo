3072e7330a9141a55258acbb723057c2d0636dff
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2026-07-26 00:12:55 +0600

    feat: add department data to home page configuration

--- src/components/home/data.ts
+++ src/components/home/data.ts
@@ -1,7 +1,13 @@
 import banner1 from '@/assets/hero/main-banner-7-desktop.webp';
 import banner2 from '@/assets/hero/5-1.webp';
 import banner3 from '@/assets/hero/6-1.webp';
-import 'swiper/css/effect-fade';
+import sofa from '@/assets/department/sofa.webp';
+import chair from '@/assets/department/chair.webp';
+import table from '@/assets/department/table.webp';
+import bed from '@/assets/department/bed.webp';
+import storage from '@/assets/department/storage.webp';
+import lighting from '@/assets/department/lightning.webp';
+
 export const heroData = [
   {
     title: 'Art Of Style',
@@ -39,5 +45,38 @@ export const heroData = [
         href: '/lanp',
       },
     ],
+  },
+];
+// department data
+export const departmentData = [
+  {
+    title: 'Sofa',
+    icon: sofa,
+    href: '/sofa',
+  },
+  {
+    title: 'Chair',
+    icon: chair,
+    href: '/chair',
+  },
+  {
+    title: 'Table',
+    icon: table,
+    href: '/table',
+  },
+  {
+    title: 'Bed',
+    icon: bed,
+    href: '/bed',
+  },
+  {
+    title: 'Storage',
+    icon: storage,
+    href: '/storage',
+  },
+  {
+    title: 'Lighting',
+    icon: lighting,
+    href: '/lighting',
   },
 ];
