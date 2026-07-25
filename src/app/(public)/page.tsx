eb926b639bca1f27b7c4051e2a7fb83a90927483
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2026-07-26 01:48:36 +0600

    feat: add SaleBanner to home page

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -2,6 +2,7 @@ import HeroSection from '@/components/home/Hero';
 import DepartmentGrid from '@/components/home/DepartmentGrid';
 import EditorialGrid from '@/components/home/EditorialGrid';
 import { DesignEditSlider } from '@/components/home/DesignEditSlider';
+import { SaleBanner } from '@/components/home/SaleBanner';
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <DepartmentGrid />
       <EditorialGrid />
       <DesignEditSlider />
+      <SaleBanner />
     </section>
   );
 }
