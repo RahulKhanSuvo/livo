9a5638fcf676d0e84eb8d9ba3af9bcd06a41c25a
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2026-07-25 23:39:51 +0600

    feat: add hero banners to home data

--- src/components/home/data.ts
+++ src/components/home/data.ts
@@ -1,7 +1,12 @@
+import banner1 from '@/assets/hero/main-banner-7-desktop.webp';
+import banner2 from '@/assets/hero/5-1.webp';
+import banner3 from '@/assets/hero/6-1.webp';
+import 'swiper/css/effect-fade';
 export const heroData = [
   {
     title: 'Art Of Style',
     subtitle: 'Design for modern living',
+    banner: banner1,
     cts: [
       {
         label: 'Explore Collections',
@@ -16,6 +21,7 @@ export const heroData = [
   {
     title: 'Urban Sophistication',
     subtitle: '',
+    banner: banner2,
     cts: [
       {
         label: 'Explore Collections',
@@ -26,6 +32,7 @@ export const heroData = [
   {
     title: 'Quite luxurious',
     subtitle: 'shape your atmosphere',
+    banner: banner3,
     cts: [
       {
         label: 'Shop Lighting',
