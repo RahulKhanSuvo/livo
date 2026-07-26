aab8d10a65550b8f9c70c1ea88c6e2041a7846fe
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2026-07-26 13:30:24 +0600

    ui: update navbar styling to include shadow

--- src/components/common/navbar/Navbar.tsx
+++ src/components/common/navbar/Navbar.tsx
@@ -10,13 +10,10 @@ import { RightActions } from './RightActions';
 export const Navbar = () => {
   const headerRef = useRef<HTMLElement>(null);
   return (
-    <header
-      ref={headerRef}
-      className="sticky top-0 z-50 w-full border-b border-neutral-200 bg-white py-2"
-    >
+    <header ref={headerRef} className="sticky top-0 z-50 w-full    bg-white py-2 shadow">
       <div className="relative">
         <Container>
-          <div className="flex h-16 items-center justify-between gap-6">
+          <div className="flex h-16 items-center justify-between gap-6 ">
             <Logo />
             <NavLinks anchor={headerRef} sideOffset={0} />
             <RightActions />
