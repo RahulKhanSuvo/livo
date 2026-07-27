bab1217145ed7007a586ab00513ca36c88cc72f2
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2026-07-27 18:46:19 +0600

    feat: create ProductList component

---
+++ src/components/shared/ProductList.tsx
@@ -0,0 +1,13 @@
+import ProductCard from '../home/ProductCard';
+import { productsData } from '../home/productsData';
+
+const ProductList = () => {
+  return (
+    <div className="grid grid-cols-3 flex-1 gap-3 ">
+      {productsData.map((item) => (
+        <ProductCard key={item.id} product={item} />
+      ))}
+    </div>
+  );
+};
+export default ProductList;
