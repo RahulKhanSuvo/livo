d6c9eae4166f9b1f480301d82d611eb6e1c7e03e
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2026-07-31 19:41:34 +0600

    feat: Add role field to user schema

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,7 @@ import { betterAuth } from 'better-auth';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
 // If your Prisma file is located elsewhere, you can change the path
 import prisma from './prisma';
+import { Role } from '@/generated/prisma/enums';
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -11,5 +12,15 @@ export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL,
   emailAndPassword: {
     enabled: true,
+  },
+  // additional data
+  user: {
+    additionalFields: {
+      role: {
+        type: 'string',
+        required: true,
+        defaultValue: Role.USER,
+      },
+    },
   },
 });
