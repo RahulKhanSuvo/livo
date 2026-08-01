09403bbfbfefb3e25b5ab23ede9b02f56556458d
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2026-08-01 20:41:51 +0600

    feat: Add Prisma seed script for admin user

---
+++ prisma/seed.ts
@@ -0,0 +1,42 @@
+import { auth } from '@/lib/auth';
+import prisma from '@/lib/prisma';
+
+async function main() {
+  const adminExists = await prisma.user.findFirst({
+    where: {
+      role: 'ADMIN',
+    },
+  });
+
+  if (adminExists) {
+    console.log('✅ Admin already exists');
+    return;
+  }
+
+  const result = await auth.api.signUpEmail({
+    body: {
+      name: 'Administrator',
+      email: 'admin@livo.com',
+      password: 'Admin@123456',
+      rememberMe: false,
+      role: 'ADMIN',
+    },
+  });
+
+  await prisma.user.update({
+    where: {
+      id: result.user.id,
+    },
+    data: {
+      role: 'ADMIN',
+    },
+  });
+
+  console.log('✅ Admin created');
+}
+
+main()
+  .catch(console.error)
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
