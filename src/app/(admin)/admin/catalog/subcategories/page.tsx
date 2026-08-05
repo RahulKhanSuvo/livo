86145d0e5b6e4928ca9f71d8e625aac8b0dc2b73
Rahul Khan suvo <rahul.khan.suvo@gmail.com>
2026-08-05 13:41:53 +0600

    refactor: Remove unused subcategories page

--- src/app/(admin)/admin/catalog/subcategories/page.tsx
+++
@@ -1,5 +0,0 @@
-import { TaxonomyExplorer } from '@/components/admin/catalog/taxonomy-explorer';
-
-export default function SubcategoriesRoute() {
-  return <TaxonomyExplorer focus="subcategory" />;
-}
