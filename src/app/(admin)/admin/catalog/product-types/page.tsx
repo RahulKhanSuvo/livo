1f0f532e241dce7c41c98ce715b916bb4d735089
Rahul Khan suvo <rahul.khan.suvo@gmail.com>
2026-08-05 13:41:48 +0600

    refactor: Remove product types page

--- src/app/(admin)/admin/catalog/product-types/page.tsx
+++
@@ -1,5 +0,0 @@
-import { TaxonomyExplorer } from '@/components/admin/catalog/taxonomy-explorer';
-
-export default function ProductTypeRoute() {
-  return <TaxonomyExplorer focus="productType" />;
-}
