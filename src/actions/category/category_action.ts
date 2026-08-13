0933f0f53c458d291518074f6361e2015e8d9a1a
Rahul Khan suvo <rahul.khan.suvo@gmail.com>
2026-08-13 13:44:05 +0600

    feat: add getProductType server action

--- src/actions/category/category_action.ts
+++ src/actions/category/category_action.ts
@@ -1,5 +1,23 @@
 'use server';
 
-export async function createCategory(payload: string) {
-  console.log(payload);
+import prisma from '@/lib/prisma';
+import { requireAdmin } from '@/lib/permissions/require-admin';
+
+export async function getProductType() {
+  await requireAdmin();
+  try {
+    const response = await prisma.productType.findMany({
+      include: {
+        subCategory: {
+          include: {
+            category: true,
+          },
+        },
+      },
+    });
+    return response;
+  } catch (error) {
+    console.error('Error: error message', error);
+    throw error;
+  }
 }
