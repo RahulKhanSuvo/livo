b0f611c0a1443eff907c5965ac4a6516b94daf47
Rahul Khan suvo <rahul.khan.suvo@gmail.com>
2026-08-14 22:19:18 +0600

    feat: implement product data table on product page

--- src/components/admin/catalog/products/ProductPageContnent.tsx
+++ src/components/admin/catalog/products/ProductPageContnent.tsx
@@ -1,13 +1,19 @@
 'use client';
 import { getAllProducts } from '@/actions/products/getAllProducts';
+import { DataTable } from '@/components/shared/data-table';
 import { useQuery } from '@tanstack/react-query';
+import { productColumns } from './columns';
 
 function ProductPageContnent() {
-  const { data: products = [] } = useQuery({
+  const { data: products } = useQuery({
     queryKey: ['products'],
-    queryFn: () => getAllProducts(),
+    queryFn: () => getAllProducts(1, 5),
   });
-  console.log('Product data', products);
-  return <>{/*<DataTable columns={[]} data={products} keyField={(r) => r.id} />*/}</>;
+  console.log(products);
+  return (
+    <>
+      <DataTable columns={productColumns} data={products?.products || []} key="product-table" />
+    </>
+  );
 }
 export default ProductPageContnent;
