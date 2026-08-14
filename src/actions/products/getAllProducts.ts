3b4a0841ecb8be6fbd2c8006960e506aa9361307
Rahul Khan suvo <rahul.khan.suvo@gmail.com>
2026-08-14 21:31:24 +0600

    feat: implement server-side pagination and include product variants in getAllProducts action

--- src/actions/products/getAllProducts.ts
+++ src/actions/products/getAllProducts.ts
@@ -1,11 +1,32 @@
 'use server';
 import prisma from '@/lib/prisma';
 
-export async function getAllProducts() {
+export async function getAllProducts(page: number = 1, limit: number = 10) {
   try {
-    const res = await prisma.product.findMany();
-    console.log('sever prove', res);
-    return res;
+    const skip = (page - 1) * limit;
+    const [products, total] = await Promise.all([
+      prisma.product.findMany({
+        take: limit,
+        skip,
+        include: {
+          variants: {
+            include: {
+              images: true,
+            },
+          },
+        },
+      }),
+      prisma.product.count(),
+    ]);
+
+    return {
+      products,
+      total,
+      page,
+      limit,
+      hasNextPage: skip + products.length < total,
+      hasPrevPage: page > 1,
+    };
   } catch (error) {
     console.log('error', error);
     throw error;
