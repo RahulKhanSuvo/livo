ec2763306a85939f50380767af90b4f7d0e988aa
Rahul Khan suvo <rahul.khan.suvo@gmail.com>
2026-08-15 12:15:48 +0600

    feat: validate product existence before update in updateProductAction

--- src/actions/products/updateProductAction.ts
+++ src/actions/products/updateProductAction.ts
@@ -1,10 +1,21 @@
 'use server';
 import { createSafeAction } from '@/lib/createSafeAction';
+import prisma from '@/lib/prisma';
 import { productValidationSchema } from './productValidation';
 
 export const updateProduct = createSafeAction(
   productValidationSchema,
   async ({ id, ...productData }) => {
+    const isProductExist = await prisma.product.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!isProductExist) {
+      throw new Error('Product not found');
+    }
+
     console.log(productData);
     return productData;
   },
