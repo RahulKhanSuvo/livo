69c5dbd77aacfb4fc1c5337409b86e2703b49a38
Rahul Khan suvo <rahul.khan.suvo@gmail.com>
2026-08-17 04:57:20 +0600

    feat: connect cart store to navbar to display dynamic item count

--- src/components/common/navbar/RightActions.tsx
+++ src/components/common/navbar/RightActions.tsx
@@ -1,13 +1,17 @@
 'use client';
-
 import { ShoppingBag01Icon } from '@hugeicons/core-free-icons';
 import { HugeiconsIcon } from '@hugeicons/react';
 import { AccountMenu } from './account-menu';
 import CartSheet from '../cart/CartSheet';
 import { useState } from 'react';
+import { useCartStore } from '@/stores/cart-store';
 
 export const RightActions = () => {
   const [cartOpen, setCartOpen] = useState(false);
+  const items = useCartStore((state) => state.items);
+  const updateQuantity = useCartStore((state) => state.updateQuantity);
+  const removeItem = useCartStore((state) => state.removeItem);
+  console.log('cart', items);
   return (
     <div className="flex items-center gap-4 sm:gap-6">
       <AccountMenu />
@@ -18,7 +22,7 @@ export const RightActions = () => {
         className="flex items-center gap-1 text-neutral-800 hover:text-black transition-colors cursor-pointer"
       >
         <HugeiconsIcon icon={ShoppingBag01Icon} size={19} strokeWidth={1.5} />
-        <span className="text-xs font-semibold ml-0.5">1</span>
+        <span className="text-xs font-semibold ml-0.5">{items?.length}</span>
       </button>
       <CartSheet isOpen={cartOpen} onClose={() => setCartOpen(false)} />
     </div>
