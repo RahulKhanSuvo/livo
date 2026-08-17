d78346c388ff5a69159bca7875d9dbb9b972eece
Rahul Khan suvo <rahul.khan.suvo@gmail.com>
2026-08-18 02:19:00 +0600

    refactor: simplify product table component structure

--- src/components/admin/catalog/products/ProductTable.tsx
+++ src/components/admin/catalog/products/ProductTable.tsx
@@ -1,19 +1,4 @@
-import { getAllProducts } from '@/actions/products/getAllProducts';
-import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
-import ProductPageContnent from './ProductPageContnent';
-
-async function ProductTable() {
-  const queryClient = new QueryClient();
-  await queryClient.prefetchQuery({
-    queryKey: ['products'],
-    queryFn: () => getAllProducts(1, 5),
-  });
-  return (
-    <>
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <ProductPageContnent />
-      </HydrationBoundary>
-    </>
-  );
+async function ProductTable({}) {
+  return <></>;
 }
 export default ProductTable;
