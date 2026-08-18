528909e90969ca04c64b8fb8cf94887ebcbca66b
Rahul Khan suvo <rahul.khan.suvo@gmail.com>
2026-08-19 00:11:01 +0600

    feat: add room hotspots server actions

---
+++ src/actions/content/room-hotspots/index.ts
@@ -0,0 +1,6 @@
+export { getRoomSceneAction } from './getRoomSceneAction';
+export { getRoomHotspotsAction } from './getRoomHotspotsAction';
+export { getAdminRoomHotspotsAction } from './getAdminRoomHotspotsAction';
+export { upsertRoomHotspotAction } from './upsertRoomHotspotAction';
+export { deleteRoomHotspotAction } from './deleteRoomHotspotAction';
+export { uploadRoomSceneImageAction } from './uploadRoomSceneImageAction';
