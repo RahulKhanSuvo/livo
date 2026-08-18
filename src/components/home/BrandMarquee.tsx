dfca9a77af733cc91bad51fdd67ed80963a8e07b
Rahul Khan suvo <rahul.khan.suvo@gmail.com>
2026-08-19 00:10:11 +0600

    ui: remove Muuto from brand marquee

--- src/components/home/BrandMarquee.tsx
+++ src/components/home/BrandMarquee.tsx
@@ -3,7 +3,6 @@ import Image from 'next/image';
 import ikeaLogo from '@/assets/mar/Group_1454_2x_ca71a0d1-52f3-4fa1-aa47-fdccb802244c.webp';
 import vitraLogo from '@/assets/mar/logo-philipp_2x_78f312c4-4d9f-41ff-aeb3-d575f6c6251f.avif';
 import hayLogo from '@/assets/mar/tradition-logo_2x_95e1e23d-c653-4fb6-9550-9fd91d283b1b.webp';
-import muutoLogo from '@/assets/mar/umbtra_2x_f72e03e7-ed78-466f-b5ee-213d2772e689.avif';
 import { Marquee } from '../ui/marquee';
 
 const brands = [
@@ -21,11 +20,6 @@ const brands = [
     id: 3,
     name: 'HAY',
     logo: hayLogo,
-  },
-  {
-    id: 4,
-    name: 'Muuto',
-    logo: muutoLogo,
   },
 ];
 
