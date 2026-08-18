63d23cdbf570b97ab81ccb2bb0b436065449c04a
Rahul Khan suvo <rahul.khan.suvo@gmail.com>
2026-08-19 00:33:18 +0600

    feat: add product slider server actions

---
+++ src/actions/content/product-slider/index.ts
@@ -0,0 +1,5 @@
+export { getProductSliderItemsAction } from './getProductSliderItemsAction';
+export { getAdminProductSliderItemsAction } from './getAdminProductSliderItemsAction';
+export { upsertProductSliderItemAction } from './upsertProductSliderItemAction';
+export { deleteProductSliderItemAction } from './deleteProductSliderItemAction';
+export { uploadProductSliderVideoAction } from './uploadProductSliderVideoAction';
