9a386b21969b642ca8072aca5e8283a4a14c9dde
Rahul Khan suvo <rahul.khan.suvo@gmail.com>
2026-08-20 00:14:32 +0600

    ui: update AddToCartButton dimensions and font size

--- src/components/home/AddToCartButton.tsx
+++ src/components/home/AddToCartButton.tsx
@@ -50,7 +50,7 @@ export function AddToCartButton({ product, selectedVariant }: AddToCartButtonPro
       <Button
         disabled={!isStockAvailable}
         variant="main"
-        className="w-[90%]"
+        className="w-[90%] h-8 text-xs lg:h-9 lg:text-sm"
         onClick={handleAddToCart}
       >
         {isStockAvailable ? 'Add to Cart' : 'Out of stock'}
