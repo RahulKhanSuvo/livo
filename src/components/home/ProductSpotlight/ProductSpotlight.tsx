7891d066eb767f7621ea5c0b830b46a4bcd5ab65
Rahul Khan suvo <rahul.khan.suvo@gmail.com>
2026-08-20 13:39:38 +0600

    ui: adjust product spotlight section heights for responsiveness

--- src/components/home/ProductSpotlight/ProductSpotlight.tsx
+++ src/components/home/ProductSpotlight/ProductSpotlight.tsx
@@ -5,7 +5,7 @@ import SpotDataGrid from './SpotDataGrid';
 
 const ProductSpotlight = () => {
   return (
-    <section className="relative h-[170vh] bg-[#f0efeb]">
+    <section className="relative h-[300vh] bg-[#f0efeb] md:h-[170vh]">
       {/* Sticky Background */}
       <div className="sticky top-0 h-[calc(120vh)] z-10">
         <Image src={second} alt="background" fill priority className="object-cover" />
@@ -13,11 +13,11 @@ const ProductSpotlight = () => {
 
       {/* Scrolling Content */}
       <div className="absolute inset-0 z-10">
-        <section className="flex h-[calc(90vh)] items-start justify-center">
+        <section className="flex h-[70vh] items-start justify-center md:h-[calc(90vh)]">
           <SpotProductTitle />
         </section>
 
-        <section className="flex h-[calc(80vh)] items-center justify-center">
+        <section className="flex h-[220vh] items-center justify-center md:h-[calc(80vh)]">
           <SpotDataGrid />
         </section>
       </div>
