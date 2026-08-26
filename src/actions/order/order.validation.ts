0566ede6a442b928e8b3d3ad963eb3efe2f99e04
Rahul Khan suvo <rahul.khan.suvo@gmail.com>
2026-08-26 13:46:52 +0600

    fix: update order validation schema constraints

--- src/actions/order/order.validation.ts
+++ src/actions/order/order.validation.ts
@@ -1,9 +1,13 @@
+import { OrderStatus } from '@/generated/prisma/enums';
 import { z } from 'zod';
 
 export const orderQuerySchema = z.object({
-  page: z.coerce.number().int().min(1).default(1),
-  limit: z.coerce.number().int().min(1).max(100).default(10),
-  status: z.string().trim().optional(),
+  page: z.coerce.number().int().min(1).default(1).optional(),
+  limit: z.coerce.number().int().min(1).max(100).default(10).optional(),
+  status: z.preprocess(
+    (val) => (val === '' ? undefined : val),
+    z.nativeEnum(OrderStatus).optional()
+  ),
   search: z.string().trim().optional(),
 });
 
