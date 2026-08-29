8f482b8312e81fc0d81bb803d3887630c24b3c4a
Rahul Khan suvo <rahul.khan.suvo@gmail.com>
2026-08-29 11:00:41 +0600

    refactor: update QueryClient to support streaming

--- src/lib/query-client.ts
+++ src/lib/query-client.ts
@@ -1,4 +1,37 @@
-import { cache } from 'react';
-import { QueryClient } from '@tanstack/react-query';
+// app/get-query-client.ts
 
-export const getQueryClient = cache(() => new QueryClient());
+import { QueryClient, defaultShouldDehydrateQuery, isServer } from '@tanstack/react-query';
+
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+
+      dehydrate: {
+        // Important for streaming
+        shouldDehydrateQuery: (query) =>
+          defaultShouldDehydrateQuery(query) || query.state.status === 'pending',
+
+        shouldRedactErrors: () => false,
+      },
+    },
+  });
+}
+
+let browserQueryClient: QueryClient | undefined;
+
+export function getQueryClient() {
+  if (isServer) {
+    // Server: new client for every request
+    return makeQueryClient();
+  }
+
+  // Browser: reuse the same client
+  if (!browserQueryClient) {
+    browserQueryClient = makeQueryClient();
+  }
+
+  return browserQueryClient;
+}
